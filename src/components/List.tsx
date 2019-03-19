0eca32266d8f14e27c4521b5eb1c09092065181d
axel10 <axel10@163.com>
2019-03-19 11:23:03 +0800

    fix bug

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,21 +1,28 @@
-import { actions } from '@/types/actions'
-import { useStateValue } from '@/utils/baseContext'
-import * as React from 'react'
+import { actions } from "../types/actions";
+import { useStateValue } from "../utils/baseContext";
+import * as React from "react";
 
 export const List = () => {
-  const [{ test: { list } }, dispatch] = useStateValue()
-  console.log(list)
+  const [
+    {
+      test: { list }
+    },
+    dispatch
+  ] = useStateValue();
+  console.log(list);
   const push = () => {
-    dispatch(actions.push('123'))
-  }
+    dispatch(actions.push("123"));
+  };
   return (
     <div>
       list
-      {
-        list.map((o, i) => {
-          return <div onClick={push} key={i}>{o}</div>
-        })
-      }
+      {list.map((o, i) => {
+        return (
+          <div onClick={push} key={i}>
+            {o}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
